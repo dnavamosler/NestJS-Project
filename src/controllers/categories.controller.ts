d3e6f73ca41b9eb23dda911fdb593e36b4d3c515
daniel <dnavamoslercl@gmail.com>
2022-04-21 10:55:50 -0400

    remove

--- src/controllers/categories.controller.ts
+++ src/controllers/categories.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('categories')
 export class CategoriesController {
-  @Get('categories/:id/products/:productId')
+  @Get(':id/products/:productId')
   getCategory(@Param('id') id: string, @Param('productId') productId: string) {
     return `Category ${id} and product ${productId}`;
   }
