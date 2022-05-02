040da9c9fcf4cccc69ef3037ac42ffecb5e8f175
daniel <dnavamoslercl@gmail.com>
2022-05-01 23:11:51 -0400

    delete users

--- src/controllers/discord.controller.ts
+++ src/controllers/discord.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import * as moment from 'moment';
 import clientGraphql from 'src/utils/graphqlClient';
 
 @Controller('discord')
@@ -6,7 +7,7 @@ export class DiscordController {
   @Post('nuevo-usuario')
   async create(@Body() payload: Respuesta) {
     /* Captura de webhook */
-    console.log(payload);
+
     if (!payload.form) {
       return { ok: true };
     }
@@ -40,6 +41,40 @@ export class DiscordController {
     return {
       message: 'Usuario almacenado correctamente!',
       response,
+    };
+  }
+
+  @Post('checkUsers')
+  async check() {
+    const { usuarios } = await clientGraphql.request(`query getUsers {
+      usuarios {
+        created_at
+        id
+      }
+    }
+    `);
+
+    const usuariosAntiguos = usuarios
+      .map((user) => ({
+        ...user,
+        semanas: moment().diff(moment(user.created_at), 'weeks'),
+      }))
+      .filter((user) => user.semanas > 8)
+      .map((user) => user.id);
+
+    await clientGraphql.request(
+      `mutation MyMutation($users: [uuid!]!) {
+      delete_usuarios(where: {id: {_in: $users}}) {
+        affected_rows
+      }
+    }
+
+    `,
+      { users: usuariosAntiguos },
+    );
+
+    return {
+      message: 'OK',
     };
   }
 }
