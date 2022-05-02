947abb6827b57de7723fb77ef1cc8af22a3c847e
daniel <dnavamoslercl@gmail.com>
2022-05-02 13:34:36 -0400

    pipes

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -6,50 +6,65 @@ import {
   HttpCode,
   HttpStatus,
   Param,
+  //ParseIntPipe,
   Post,
   Put,
   Query,
 } from '@nestjs/common';
+import { ParseIntPipe } from '../common/parse-int.pipe';
+import { ProductsService } from '../services/products.service';
 
 @Controller('products')
 export class ProductsController {
+  /* Asi se inyecta un servicio */
+  constructor(private productsService: ProductsService) {}
+
   @Get('')
   getProducts(
     @Query('limit') limit = 100,
     @Query('offset') offset = 0,
     @Query('brand') brand: string,
   ) {
-    return { limit, offset, brand };
+    // return { limit, offset, brand };
+
+    return this.productsService.findAll();
   }
   /* Ruta con parametros */
   @Get(':id')
   @HttpCode(HttpStatus.OK)
-  getProduct(@Param('id') id: string) {
-    return `Product ${id}`;
+  /* El parseIntPipe nos transforma a int.. */
+  getProduct(@Param('id', ParseIntPipe) id: number) {
+    /* con un + --> lo pasa directo a number */
+    return this.productsService.findOne(id);
   }
 
   @Post()
   create(@Body() payload: any) {
-    return {
+    /*  return {
       message: 'acción de crear',
       payload,
-    };
+    }; */
+    return this.productsService.create(payload);
   }
 
   @Put(':id')
-  update(@Param('id') id: number, @Body() payload: any) {
-    return {
+  update(@Param('id', ParseIntPipe) id: number, @Body() payload: any) {
+    /*  return {
       message: 'acción de actualización',
       payload,
       id,
-    };
+    }; */
+
+    return this.productsService.update(id, payload);
   }
 
   @Delete(':id')
-  delete(@Param('id') id: number) {
-    return {
+  delete(@Param('id', ParseIntPipe) id: number) {
+    /*  return {
       message: 'acción de delete',
       id,
-    };
+    }; */
+
+    return this.productsService.delete(id);
   }
 }
---
+++ src/services/products.service.ts
@@ -0,0 +1,76 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Product } from 'src/entities/product.entity';
+
+/* Este servicio es para administrar productos */
+@Injectable()
+export class ProductsService {
+  private counterId = 1;
+  /* Array cargado en memoria... */
+  private products: Product[] = [
+    {
+      id: 1,
+      name: 'Product 1',
+      description: 'Bla Bla',
+      price: 122,
+      stock: 12,
+      image: '',
+    },
+  ];
+
+  findAll() {
+    return this.products;
+  }
+
+  findOne(id: number) {
+    const product = this.products.find((product) => product.id === id);
+
+    if (!product) {
+      throw new NotFoundException(`Product #${id} not found`);
+    }
+
+    return product;
+  }
+
+  create(payload: any) {
+    this.counterId = this.counterId + 1;
+    const newProduct = {
+      id: this.counterId,
+      ...payload,
+    };
+
+    this.products.push(newProduct);
+
+    return newProduct;
+  }
+  update(id: number, payload: any) {
+    const oldProduct = this.findOne(id);
+    const newProduct = {
+      ...oldProduct,
+      ...payload,
+    };
+
+    if (!oldProduct) {
+      throw new NotFoundException(`Product #${id} not found`);
+    }
+
+    this.products = this.products.map((p) => {
+      if (p.id === id) {
+        return newProduct;
+      } else {
+        return p;
+      }
+    });
+
+    return { ok: true };
+  }
+  delete(id: number) {
+    const deletedProduct = this.findOne(id);
+    if (!deletedProduct) {
+      throw new NotFoundException(`Product #${id} not found`);
+    }
+
+    this.products = this.products.filter((p) => p.id !== id);
+
+    return { ok: true };
+  }
+}
