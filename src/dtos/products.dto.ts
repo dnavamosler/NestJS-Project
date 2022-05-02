9a3c8fc01478c6a4b0d50b51ae0e3f42680b8a4b
daniel <dnavamoslercl@gmail.com>
2022-05-02 14:15:54 -0400

    curso finalizado

--- src/dtos/products.dto.ts
+++ src/dtos/products.dto.ts
@@ -1,14 +1,45 @@
+import {
+  IsString,
+  IsNumber,
+  IsUrl,
+  IsNotEmpty,
+  IsPositive,
+} from 'class-validator';
+
+import { PartialType } from '@nestjs/mapped-types';
+
 export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
   readonly name: string;
+  @IsString()
+  @IsNotEmpty()
   readonly description: string;
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
   readonly price: number;
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
   readonly stock: number;
+  @IsUrl()
+  @IsNotEmpty()
   readonly image: string;
 }
-export class UpdateProductDto {
+
+/* Va a utilizar las mismas validaciones pero las pondra opcionales */
+export class UpdateProductDto extends PartialType(CreateProductDto) {
+  /* @IsString()
   readonly name?: string;
+  @IsString()
   readonly description?: string;
+  @IsNumber()
+  @IsPositive()
   readonly price?: number;
+  @IsNumber()
+  @IsPositive()
   readonly stock?: number;
-  readonly image?: string;
+  @IsUrl()
+  readonly image?: string; */
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist:
+        true /* Automaticamente niega los atributos que no esten definidos en el DTO */,
+      forbidNonWhitelisted:
+        true /* Le coloca un error si se añaden datos adicionales */,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
