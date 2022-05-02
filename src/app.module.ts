1fdad850126f610ed267fd654f871658de455fd2
daniel <dnavamoslercl@gmail.com>
2022-05-02 12:25:07 -0400

    eliminar adicionales

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsController } from './controllers/products.controller';
 import { CategoriesController } from './controllers/categories.controller';
-import { DiscordController } from './controllers/discord.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, ProductsController, CategoriesController, DiscordController],
+  controllers: [AppController, ProductsController, CategoriesController],
   providers: [AppService],
 })
 export class AppModule {}
